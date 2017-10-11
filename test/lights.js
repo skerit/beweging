05d538cb31f15d56f37b8ce00e5e7aece14e8d29
Jelle De Loecker <jelle@develry.be>
2017-10-11 15:30:18 +0200

    Fix consecutive stillness not being used + add new tests

--- test/lights.js
+++ test/lights.js
@@ -11,6 +11,59 @@ describe('Lights', function() {
 
 	let beweging = new Beweging.Beweging();
 
+	it('should not emit motion when lights go out after movement', function lightoutAfterMotion(done) {
+
+		var source = libpath.resolve(__dirname, '..', 'samples', 'lightout-after-movement.h264'),
+		    proc,
+		    lights = 0,
+		    movement = 0,
+		    motion_end = 0,
+		    first_movement = null,
+		    last_movement = null,
+		    first_light = null,
+		    last_light = null;
+
+		proc = beweging.detectStream(source);
+
+		proc.on('motion', function gotMotion(motion) {
+			if (first_movement == null) {
+				first_movement = motion.frame;
+			} else {
+				last_movement = motion.frame;
+			}
+			movement++;
+			//console.log('Motion on frame', motion.frame, 'Lightness:', ~~motion.average);
+			//throw new Error('Light was detected as motion');
+		});
+
+		proc.on('light', function gotLights(jitter) {
+			if (first_light == null) {
+				first_light = jitter.frame;
+			} else {
+				last_light = jitter.frame;
+			}
+
+			lights++;
+		});
+
+		proc.on('motion_end', function gotEnd(end) {
+			motion_end++;
+		});
+
+		proc.on('end', function onEnd() {
+			assert.equal(movement > 0, true, 'No movement was detected');
+			assert.equal(first_movement > 40 && first_movement < 45, true, 'First movement was not when expected. Should be around frame 40, but was: ' + first_movement);
+			assert.equal(last_movement < 85, true, 'Last movement was not when expected. Should be before frame 85, but was: ' + last_movement);
+			assert.equal(motion_end > 0, true, 'No motion_end event was emitted');
+
+			// The first light event is actually just after the first movement event (when the door opens, the light goes on)
+			// but by 'lights' we actually mean 'only lights', so ...
+			assert.equal(first_light > 85, true, 'First light should have been after frame 85, but was ' + first_light);
+			assert.equal(lights > 2, true, 'No lights were detected');
+			done();
+		});
+	});
+
 	it('should not emit motion when lights alternate from light to dark', function lightOn(done) {
 
 		var source = libpath.resolve(__dirname, '..', 'samples', 'alternating-light2.h264'),
